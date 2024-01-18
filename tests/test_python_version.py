4a2ee185238bec9a3743726cd837d7b7cb0d07e2
David Evans <dave.evans@thedatalab.org>
2024-01-18 11:52:24 +0000

    Enforce consistent Python version
    There isn't a sensible way we can DRY out this configuration, so we do
    the next best thing which is enforcing that we repeat ourselves
    consistently.

---
+++ tests/test_python_version.py
@@ -0,0 +1,34 @@
+"""
+Ensure all the various places in which we need to reference our target Python version
+are consistent.
+"""
+import re
+from pathlib import Path
+
+import pytest
+
+
+BASE_DIR = Path(__file__).parents[1]
+
+
+@pytest.fixture(scope="session")
+def python_version():
+    # We treat the `.python-version` file as the canonical specifier
+    return (BASE_DIR / ".python-version").read_text().strip()
+
+
+def test_justfile(python_version):
+    contents = (BASE_DIR / "justfile").read_text()
+    match = re.search(r"DEFAULT_PYTHON.*python([\d\.]+)", contents)
+    assert match.group(1) == python_version
+
+
+@pytest.mark.parametrize(
+    "filename",
+    BASE_DIR.glob(".github/workflows/*.yml"),
+    ids=lambda path: path.name,
+)
+def test_github_workflows(filename, python_version):
+    contents = filename.read_text()
+    for match in re.findall(r"python-version:.*?([\d\.]+)", contents):
+        assert match == python_version
