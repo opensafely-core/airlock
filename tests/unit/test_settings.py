32fd12417227a37d9fa15d00e6cfbe9ac529c7ed
David Evans <dave.evans@thedatalab.org>
2024-01-18 15:30:24 +0000

    Add helpful error message for missing env vars

--- tests/unit/test_settings.py
+++ tests/unit/test_settings.py
@@ -1,6 +1,16 @@
+import pytest
 from django.conf import settings
+
+from airlock import settings as settings_funcs
 
 
 # TODO: Stub test to get us started with
 def test_secret_key():
     assert len(settings.SECRET_KEY) > 0
+
+
+def test_get_env_var():
+    with pytest.raises(
+        RuntimeError, match="Missing environment variable: AINT_NO_SUCH_VAR"
+    ):
+        settings_funcs.get_env_var("AINT_NO_SUCH_VAR")
