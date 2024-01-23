d94057ee595d083e04fc53a173cd5fa6ff30eabb
David Evans <dave.evans@thedatalab.org>
2024-01-23 11:55:54 +0000

    Add very basic file browser view

--- airlock/views.py
+++ airlock/views.py
@@ -1,5 +1,24 @@
+from django.http import Http404
+from django.shortcuts import redirect
 from django.template.response import TemplateResponse
+
+from airlock.workspace_api import PathItem
 
 
 def index(request):
     return TemplateResponse(request, "index.html")
+
+
+def file_browser(request, path: str = ""):
+    path_item = PathItem.from_relative_path(path)
+
+    if not path_item.exists():
+        raise Http404()
+
+    is_directory_url = path.endswith("/") or path == ""
+    if path_item.is_directory() != is_directory_url:
+        return redirect(path_item.url())
+
+    return TemplateResponse(
+        request, "file_browser/index.html", {"path_item": path_item}
+    )
--- tests/integration/test_views.py
+++ tests/integration/test_views.py
@@ -1,3 +1,50 @@
+import pytest
+
+
 def test_index(client):
     response = client.get("/")
     assert "Hello World" in response.rendered_content
+
+
+@pytest.fixture
+def tmp_workspace(tmp_path, settings):
+    settings.WORKSPACE_DIR = tmp_path
+    return tmp_path
+
+
+def test_file_browser_index(client, tmp_workspace):
+    (tmp_workspace / "file.txt").touch()
+    response = client.get("/file-browser/")
+    assert "file.txt" in response.rendered_content
+
+
+def test_file_browser_with_directory(client, tmp_workspace):
+    (tmp_workspace / "some_dir").mkdir()
+    (tmp_workspace / "some_dir/file.txt").touch()
+    response = client.get("/file-browser/some_dir/")
+    assert "file.txt" in response.rendered_content
+
+
+def test_file_browser_with_file(client, tmp_workspace):
+    (tmp_workspace / "file.txt").write_text("foobar")
+    response = client.get("/file-browser/file.txt")
+    assert "foobar" in response.rendered_content
+
+
+def test_file_browser_with_404(client, tmp_workspace):
+    response = client.get("/file-browser/no_such_file.txt")
+    assert response.status_code == 404
+
+
+def test_file_browser_redirects_to_directory(client, tmp_workspace):
+    (tmp_workspace / "some_dir").mkdir()
+    response = client.get("/file-browser/some_dir")
+    assert response.status_code == 302
+    assert response.headers["Location"] == "/file-browser/some_dir/"
+
+
+def test_file_browser_redirects_to_file(client, tmp_workspace):
+    (tmp_workspace / "file.txt").touch()
+    response = client.get("/file-browser/file.txt/")
+    assert response.status_code == 302
+    assert response.headers["Location"] == "/file-browser/file.txt"
