326faa50fa4e566b6dbd901796115a80fff06b32
Tom Ward <tomward@fmail.co.uk>
2024-01-24 16:58:19 +0000

    fix: don't run playwright tests inside docker test
    * we can remove this when the file permissions are fixed
    * here is the issue:
    
    ```
    % docker compose run test /opt/venv/bin/python -m playwright install
    ...
    Failed to install browsers
    Error: EACCES: permission denied, mkdir '/.cache'
    ```

--- tests/functional/test_page.py
+++ tests/functional/test_page.py
@@ -1,6 +1,8 @@
+import pytest
 from playwright.sync_api import expect
 
 
+@pytest.mark.playwright
 def test_homepage_can_load(page, live_server):
     page.goto(live_server.url)
     expect(page.get_by_role("heading", name="Airlock")).to_be_visible()
