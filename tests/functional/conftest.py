c76b21d9b5ccefecb7ed0bf6bfa018e985f09bf2
Becky Smith <becky.smith@thedatalab.org>
2024-02-02 16:54:22 +0000

    Add a functional tests fixtures for a logged in users
    We don't need to test the full login flow again, but we will need
    logged in users for future functional tests. This adds a helper
    fixture, `login_as_user`, and 2 fixtures using it to mock an
    output-checker and a researcher user, to create session data and
    set expected cookies in the browser context.
    
    The workspace_index_page tests don't test anything special, functional-
    test wise, they just test that the fixture works to load a page as
    an expected logged in user.

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -3,6 +3,8 @@ import subprocess
 import sys
 
 import pytest
+from django.conf import settings
+from django.contrib.sessions.models import Session
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -25,3 +27,53 @@ def playwright_install(request):
         command.extend(["--with-deps"])
     with capmanager.global_and_fixture_disabled():
         subprocess.run(command, check=True)
+
+
+TEST_USERS = {
+    "test_output_checker": {
+        "id": "test_output_checker",
+        "username": "test_output_checker",
+        "workspaces": [],
+        "output_checker": True,
+    },
+    "test_researcher": {
+        "id": "test_researcher",
+        "username": "test_researcher",
+        "workspaces": ["test-dir1"],
+        "output_checker": False,
+    },
+}
+
+
+@pytest.fixture
+def login_as_user(live_server, context):
+    """
+    Fixture that creates a session with relevant user data and
+    sets the session cookie.
+    """
+
+    def _login(username):
+        session_store = Session.get_session_store_class()()
+        session_store["user"] = TEST_USERS[username]
+        session_store.save()
+        context.add_cookies(
+            [
+                {
+                    "name": settings.SESSION_COOKIE_NAME,
+                    "value": session_store._session_key,
+                    "url": live_server.url,
+                }
+            ]
+        )
+
+    return _login
+
+
+@pytest.fixture
+def output_checker_user(login_as_user):
+    yield login_as_user("test_output_checker")
+
+
+@pytest.fixture
+def researcher_user(login_as_user):
+    yield login_as_user("test_researcher")
