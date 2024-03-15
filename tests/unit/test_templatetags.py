e4bbf4df5bf95046329e8f11cabd73ab8435e75a
bloodearnest <simon.davy@thedatalab.org>
2024-03-15 11:52:57 +0000

    Remove templatetags as no longer used

--- tests/unit/test_templatetags.py
+++
@@ -1,9 +0,0 @@
-from airlock.templatetags.airlocktags import as_csv_data
-
-
-def test_file_content_as_csv_data():
-    content = "Header1,Header2,Header3\n" "One,Two,Three\n" "Four,Five,Six\n"
-    assert as_csv_data(content) == {
-        "headers": ["Header1", "Header2", "Header3"],
-        "rows": [["One", "Two", "Three"], ["Four", "Five", "Six"]],
-    }
