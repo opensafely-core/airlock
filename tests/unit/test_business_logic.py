7323302edda9d69b9a3e625cf1b9ea1da5147084
Becky Smith <becky.smith@thedatalab.org>
2024-03-26 13:14:55 +0000

    Prevent ReleaseRequest approval until all request files have two approvals

--- tests/unit/test_business_logic.py
+++ tests/unit/test_business_logic.py
@@ -89,7 +89,7 @@ def mock_old_api(monkeypatch):
     monkeypatch.setattr(old_api, "upload_file", MagicMock(autospec=old_api.upload_file))
 
 
-def test_provider_request_release_files_not_approved():
+def test_provider_request_release_files_request_not_approved():
     author = factories.create_user("author", ["workspace"])
     checker = factories.create_user("checker", [], output_checker=True)
     release_request = factories.create_release_request(
@@ -118,8 +118,11 @@ def test_provider_request_release_files_invalid_file_type():
     # request
     relpath = Path("test/file.foo")
     with patch("airlock.utils.LEVEL4_FILE_TYPES", [".foo"]):
-        factories.write_request_file(release_request, "group", relpath, "test")
+        factories.write_request_file(
+            release_request, "group", relpath, "test", approved=True
+        )
 
+    release_request = factories.refresh_release_request(release_request)
     factories.bll.set_status(release_request, RequestStatus.APPROVED, checker)
     bll = BusinessLogicLayer(data_access_layer=None)
     with pytest.raises(bll.RequestPermissionDenied):
@@ -137,7 +140,9 @@ def test_provider_request_release_files(mock_old_api):
         status=RequestStatus.SUBMITTED,
     )
     relpath = Path("test/file.txt")
-    factories.write_request_file(release_request, "group", relpath, "test")
+    factories.write_request_file(
+        release_request, "group", relpath, "test", approved=True
+    )
     # Add a supporting file, which should NOT be released
     supporting_relpath = Path("test/supporting_file.txt")
     factories.write_request_file(
@@ -331,6 +336,26 @@ def test_set_status(current, future, valid_author, valid_checker, bll):
     else:
         with pytest.raises((bll.InvalidStateTransition, bll.RequestPermissionDenied)):
             bll.set_status(release_request2, future, user=checker)
+
+
+@pytest.mark.parametrize("files_approved", (True, False))
+def test_set_status_approved(files_approved, bll):
+    author = factories.create_user("author", ["workspace"], False)
+    checker = factories.create_user("checker", [], True)
+    release_request = factories.create_release_request(
+        "workspace", user=author, status=RequestStatus.SUBMITTED
+    )
+    factories.write_request_file(
+        release_request, "group", "test/file.txt", approved=files_approved
+    )
+    release_request = factories.refresh_release_request(release_request)
+
+    if files_approved:
+        bll.set_status(release_request, RequestStatus.APPROVED, user=checker)
+        assert release_request.status == RequestStatus.APPROVED
+    else:
+        with pytest.raises((bll.InvalidStateTransition, bll.RequestPermissionDenied)):
+            bll.set_status(release_request, RequestStatus.APPROVED, user=checker)
 
 
 def test_set_status_cannot_action_own_request(bll):
