46ec3d544916a6c7c1e7fabd032cca82767a5a63
bloodearnest <simon.davy@thedatalab.org>
2024-03-27 12:44:33 +0000

    Periodically refresh the authorization information for users
    This means we get a fresh list of workspace permissions, so users don't
    have to log out and in again to pick up changes

---
+++ tests/integration/test_middleware.py
@@ -0,0 +1,53 @@
+import time
+
+import pytest
+from django.conf import settings
+
+from tests import factories
+
+
+@pytest.mark.django_db
+def test_middleware_expired_user(airlock_client, responses):
+    user = factories.create_user()
+    airlock_client.login_with_user(user)
+    factories.create_workspace("new_workspace")
+
+    response = airlock_client.get("/workspaces/view/new_workspace/")
+    assert response.status_code == 403
+
+    # skip some time
+    session = airlock_client.session
+    session["user"]["last_refresh"] = time.time() - (2 * settings.AIRLOCK_AUTHZ_TIMEOUT)
+    session.save()
+
+    new_workspaces = user.workspaces.copy()
+    new_workspaces["new_workspace"] = {"project": "other_project"}
+
+    responses.post(
+        f"{settings.AIRLOCK_API_ENDPOINT}/releases/authorise",
+        status=200,
+        json={
+            "username": user.username,
+            "output_checker": user.output_checker,
+            "workspaces": new_workspaces,
+        },
+    )
+
+    response = airlock_client.get("/workspaces/view/new_workspace/")
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_middleware_expired_error(airlock_client, responses):
+    last_refresh = time.time() - (2 * settings.AIRLOCK_AUTHZ_TIMEOUT)
+    user = factories.create_user(last_refresh=last_refresh)
+    airlock_client.login_with_user(user)
+    factories.create_workspace("new_workspace")
+
+    responses.post(
+        f"{settings.AIRLOCK_API_ENDPOINT}/releases/authorise",
+        status=500,
+    )
+
+    response = airlock_client.get("/workspaces/")
+    assert response.status_code == 200
