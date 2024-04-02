7a61f5409a9a9e678b9ec062f9065f53b820d17d
Becky Smith <becky.smith@thedatalab.org>
2024-04-02 16:54:24 +0100

    Show files beginning with . as invalid

--- airlock/utils.py
+++ airlock/utils.py
@@ -4,4 +4,4 @@ from pipeline.constants import LEVEL4_FILE_TYPES
 
 
 def is_valid_file_type(path: Path):
-    return path.suffix in LEVEL4_FILE_TYPES
+    return not path.name.startswith(".") and path.suffix in LEVEL4_FILE_TYPES
