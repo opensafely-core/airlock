331d09871e4e658fc2aafb096f381e738dd26f27
Tom Ward <tomward@fmail.co.uk>
2024-04-05 16:59:57 +0100

    fixup! Don't offer to add a file as withdrawn

--- airlock/forms.py
+++ airlock/forms.py
@@ -14,7 +14,9 @@ class AddFileForm(forms.Form):
     filetype = forms.ChoiceField(
         required=True,
         choices=[
-            (i.name, i.name.title()) for i in RequestFileType if i.name != "WITHDRAWN"
+            (i.name, i.name.title())
+            for i in RequestFileType
+            if i != RequestFileType.WITHDRAWN
         ],
         initial=RequestFileType.OUTPUT.name,
     )
