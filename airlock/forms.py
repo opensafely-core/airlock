581655ed01bf8b3c7ed93dfb8381d19c1b3e29d5
bloodearnest <simon.davy@thedatalab.org>
2024-05-16 16:52:46 +0100

    Rework adding files flow to support multiple files.
     - make the add file Form support a dynamic number of files, and rework
       the POST view to handle this.
     - have the add file button hx-post to a new multiselect view with just
       the one file
     - this endpoint dynamically creates and renders the add file form, with
       one row per file submitted.
     - HTMX renders the dialog, user submits to old url.
    
    Other than a slight reformatting of the form, the flow should look and
    feel identical to before. However, we can now submit multiple files at
    the same time using this new plumbing.

--- airlock/forms.py
+++ airlock/forms.py
@@ -3,27 +3,42 @@ from django import forms
 from airlock.business_logic import FileGroup, RequestFileType
 
 
+class ListField(forms.Field):
+    # adding a multiselect widget means Django magically inferes that this is
+    # multi value field, and parses values into a list.
+    #
+    # We don't even use the widget to render the form at any point.
+    widget = forms.SelectMultiple
+
+
 class TokenLoginForm(forms.Form):
     user = forms.CharField()
     token = forms.CharField()
 
 
-class AddFileForm(forms.Form):
+class MultiselectForm(forms.Form):
+    """This is used to perform an action on multiple files."""
+
+    action = forms.CharField()  # which submit button was used
+    next_url = InternalRedirectField()  # where do we return to when complete
+    selected = ListField()  # the list of files selected
+
+
+class AddFilesForm(forms.Form):
+    FILETYPE_CHOICES = [
+        (RequestFileType.OUTPUT.name, RequestFileType.OUTPUT.name.title()),
+        (RequestFileType.SUPPORTING.name, RequestFileType.SUPPORTING.name.title()),
+    ]
+
     filegroup = forms.ChoiceField(required=False)
     new_filegroup = forms.CharField(required=False)
-    filetype = forms.ChoiceField(
-        required=True,
-        choices=[
-            (i.name, i.name.title())
-            for i in RequestFileType
-            if i in [RequestFileType.OUTPUT, RequestFileType.SUPPORTING]
-        ],
-        initial=RequestFileType.OUTPUT.name,
-    )
+    next_url = forms.CharField(required=False)
 
     def __init__(self, *args, **kwargs):
         release_request = kwargs.pop("release_request")
+        self.files_to_add = kwargs.pop("files", [])
         super().__init__(*args, **kwargs)
+
         if release_request:
             self.filegroup_names = release_request.filegroups.keys()
         else:
@@ -35,6 +50,21 @@ class AddFileForm(forms.Form):
         self.fields["filegroup"].choices = group_choices
         self.fields["new_filegroup"]
 
+        # dynamically add 2 enumerated fields per file
+        for i, filename in enumerate(self.files_to_add):
+            # filename as hidden field
+            self.fields[f"file_{i}"] = forms.CharField(
+                required=True,
+                initial=filename,
+                widget=forms.HiddenInput(),
+            )
+            # filetype for this file
+            self.fields[f"filetype_{i}"] = forms.ChoiceField(
+                choices=self.FILETYPE_CHOICES,
+                required=True,
+                initial=RequestFileType.OUTPUT.name,
+            )
+
     def clean_new_filegroup(self):
         new_filegroup = self.cleaned_data.get("new_filegroup", "").lower()
         if new_filegroup in [fg.lower() for fg in self.filegroup_names]:
@@ -44,6 +74,14 @@ class AddFileForm(forms.Form):
             )
         else:
             return new_filegroup
+
+    def file_fields(self):
+        """Template helper to loop through each files fields."""
+        for i, filename in enumerate(self.files_to_add):
+            yield {
+                "file": self.fields[f"file_{i}"],
+                "filetype": self.fields[f"filetype_{i}"],
+            }
 
 
 class GroupEditForm(forms.Form):
