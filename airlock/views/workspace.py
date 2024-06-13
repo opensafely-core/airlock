a68fe22568d086f89dd334ae6fa0c7a6eaa84854
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2024-06-13 15:27:44 +0100

    Only show the download button in certain contexts

--- airlock/views/workspace.py
+++ airlock/views/workspace.py
@@ -136,6 +136,7 @@ def workspace_view(request, workspace_name: str, path: str = ""):
             "code_url": code_url,
             "return_url": "",
             "is_output_checker": request.user.output_checker,
+            # "is_author": False
         },
     )
 
