a42e1b8462689cc28bcfe102ef5a76eb72af2228
Tom Ward <tomward@fmail.co.uk>
2024-08-15 16:23:33 +0100

    Give some type hints about forms

--- tests/unit/test_forms.py
+++ tests/unit/test_forms.py
@@ -10,7 +10,7 @@ pytestmark = pytest.mark.django_db
 
 def test_add_files_form_no_release_request():
     form = AddFileForm(release_request=None)
-    # Use type narrowing to persuade mpy this has a `choices` attr
+    # Use type narrowing to persuade mypy this has a `choices` attr
     assert isinstance(form.fields["filegroup"], forms.ChoiceField)
     assert form.fields["filegroup"].choices == [("default", "default")]
 
@@ -37,6 +37,8 @@ def test_filetype_formset():
 def test_add_files_form_empty_release_request():
     release_request = factories.create_release_request("workspace")
     form = AddFileForm(release_request=release_request)
+    # Use type narrowing to persuade mypy this has a `choices` attr
+    assert isinstance(form.fields["filegroup"], forms.ChoiceField)
     assert form.fields["filegroup"].choices == [("default", "default")]
 
 
@@ -50,6 +52,8 @@ def test_add_files_form_filegroup_choices():
     factories.create_filegroup(other_release_request, "other_group")
 
     form = AddFileForm(release_request=release_request)
+    # Use type narrowing to persuade mypy this has a `choices` attr
+    assert isinstance(form.fields["filegroup"], forms.ChoiceField)
     # default group is always first, other choices are sorted
     assert form.fields["filegroup"].choices == [
         ("default", "default"),
