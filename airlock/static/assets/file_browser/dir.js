c0cbe5c59b3fc4a2f7d7e0fbfd011a28ca94278a
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2024-09-12 11:39:38 +0100

    Simplify dir script for based on datatables

--- airlock/static/assets/file_browser/dir.js
+++ airlock/static/assets/file_browser/dir.js
@@ -1,13 +1,11 @@
-// ensure datatable is initialised when loading over HTMX
-window.initCustomTable ? window.initCustomTable() : null;
-
 // implement select all checkbox
-function toggleSelectAll(elem, event) {
+function toggleSelectAll(elem) {
     const form = document.querySelector("#multiselect_form");
 
-    const checkboxes = form.querySelectorAll('input[type="checkbox"]');
+    /** @type {NodeListOf<HTMLInputElement>|undefined} */
+    const checkboxes = form?.querySelectorAll('input[type="checkbox"]');
 
-    checkboxes.forEach(function(checkbox) {
+    checkboxes?.forEach(function(checkbox) {
         checkbox.checked = elem.checked;
     });
 }
