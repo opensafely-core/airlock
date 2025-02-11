69be152737788ccd2c9a9bca678c4e71f6f15736
Becky Smith <becky.smith@thedatalab.org>
2025-02-11 11:27:55 +0000

    Run file uploader with run-one-constantly

--- docker/entrypoints/prod.sh
+++ docker/entrypoints/prod.sh
@@ -7,4 +7,6 @@ export PYTHONUNBUFFERED=TRUE  # make sure the log output lines don't clobber eac
 ./manage.py check --deploy
 ./manage.py migrate
 
-./manage.py run_file_uploader & exec "$@"
+run-one-constantly ./manage.py run_file_uploader &
+
+exec "$@"
