e87125d803796678058ca0fb99d8cee9c893fc76
Becky Smith <becky.smith@thedatalab.org>
2025-03-07 09:38:31 +0000

    More robust regex and tests for it

--- tests/integration/test_screenshots_in_docs.py
+++ tests/integration/test_screenshots_in_docs.py
@@ -2,10 +2,11 @@ import os
 import re
 from pathlib import Path
 
+import pytest
 from django.conf import settings
 
 
-SCREENSHOT_RE = re.compile(r"\b[\w-]+.png\b")
+SCREENSHOT_RE = re.compile(r"\/([^\/]+\.png\b)")
 
 
 def test_docs_screenshots_are_used():
@@ -29,3 +30,33 @@ def test_docs_screenshots_are_used():
     assert not not_found_screenshots, (
         f"Missing screenshot files referenced in docs: {not_found_screenshots}"
     )
+
+
+@pytest.mark.parametrize(
+    "text_snippet,found",
+    [
+        ("../screenshots/screenshot.png", ["screenshot.png"]),
+        ("../screenshots/a-screenshot.png", ["a-screenshot.png"]),
+        ("../screenshots/a.test.screenshot.png", ["a.test.screenshot.png"]),
+        ("../screenshots/screenshot(1).png", ["screenshot(1).png"]),
+        ("../screenshots/subdir/1screenshot.png", ["1screenshot.png"]),
+        ("/screenshots/subdir/subsubdir/screenshot.png", ["screenshot.png"]),
+        (
+            "some text dir/screenshot.png some text another/dir/screenshot1.png",
+            ["screenshot.png", "screenshot1.png"],
+        ),
+        (
+            """            Markdown                               |  Rendered
+            :-------------------------:                            |:-------------------------:
+            ![Markdown](../screenshots/mkd_screenshot.png) | ![Rendered](../screenshots/mkd_screenshot1.png))
+                    )
+            """,
+            ["mkd_screenshot.png", "mkd_screenshot1.png"],
+        ),
+        ("/dir/screenshot.png", ["screenshot.png"]),
+        ("screenshot.png", []),
+        ("/screenshots/subdir/screenshot.png1", []),
+    ],
+)
+def test_screenshot_png_detection(text_snippet, found):
+    assert SCREENSHOT_RE.findall(text_snippet) == found
