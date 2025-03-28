dd9dbbde5105962540d3b5df1b2e89e05aef078a
Becky Smith <becky.smith@thedatalab.org>
2025-03-28 08:35:51 +0000

    Add a failing test for the last modified bug
    Test that the etag (cache id) changes when the template for a
    renderer is updated, but the last modified date stays the same.
    We expect the template change to be picked up and the file reloaded.

--- tests/integration/views/test_helpers.py
+++ tests/integration/views/test_helpers.py
@@ -1,7 +1,14 @@
+import os
+from email.utils import formatdate
+
 from django.contrib.messages.api import get_messages
 from django.contrib.messages.storage.session import SessionStorage
 from django.contrib.sessions.backends.db import SessionStore
-from django.template import Context, Template
+from django.template import (  # type: ignore
+    Context,
+    Template,
+    autoreload,
+)
 from django.test import RequestFactory
 
 from airlock import forms, renderers
@@ -35,6 +42,76 @@ def test_serve_file(tmp_path, rf):
     )
     response = helpers.serve_file(request, renderer)
     assert response.status_code == 200
+
+
+def test_serve_file_template_changes(tmp_path, rf, settings):
+    settings.TEMPLATES = [
+        {
+            "BACKEND": "django.template.backends.django.DjangoTemplates",
+            "DIRS": [tmp_path],
+            "APP_DIRS": False,
+            "OPTIONS": {"debug": True},
+        },
+    ]
+
+    template_file = tmp_path / "test_template.html"
+    template_file.write_text("{{ text }}")
+
+    class TestRenderer(renderers.TextRenderer):
+        template = renderers.RendererTemplate.from_name("test_template.html")
+
+    test_file = tmp_path / "test.foo"
+    test_file.write_text("foo")
+
+    # set the time on both the test and template file to 03 Jan 2025
+    time = 1735900200
+    os.utime(test_file, (time, time))
+    os.utime(template_file, (time, time))
+
+    renderer = TestRenderer.from_file(test_file)
+    assert renderer.last_modified == formatdate(time, usegmt=True)
+
+    request = rf.get("/")
+    response = helpers.serve_file(request, renderer)
+    assert response.status_code == 200
+    assert response.rendered_content == "foo"
+
+    # update the template
+    template_file.write_text("{{ text }}123")
+
+    # Clear the template cache. In a development environment, django's template
+    # autoreloader would do this for us
+    # https://github.com/django/django/blob/5.1.7/django/template/autoreload.py#L55
+    # In production, it's irrelevant, because the template cache is in-memory, and
+    # templates won't change without a deploy and app restart
+    autoreload.reset_loaders()
+
+    # serve_file() is called from a workspace or request content view, where the
+    # renderer is instantiated again from the workspace or request file
+    # A new call to serve_file() will be called with a new renderer, so it will
+    # include any changes to the renderer's cache ID or last_modified date
+    new_renderer = TestRenderer.from_file(test_file)
+    # The etag is the combined cache ID based on both file and template; it has
+    # changed because the template has been updated
+    assert new_renderer.etag != renderer.etag
+    # last_modified is based on the file content only, so has not changed
+    assert new_renderer.last_modified == renderer.last_modified
+
+    request = rf.get(
+        "/",
+        # headers with etag and last_modified from the renderer used in the
+        # first request
+        headers={
+            "If-None-Match": renderer.etag,
+            "If-Modified-Since": renderer.last_modified,
+        },
+    )
+
+    # The template has changed, so we expect the file to be reloaded, and
+    # the content to reflect the updated template
+    response = helpers.serve_file(request, new_renderer)
+    assert response.status_code == 200
+    assert response.rendered_content == "foo123"
 
 
 def test_display_form_errors():
