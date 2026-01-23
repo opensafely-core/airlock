dde931011afad870493e5c807bdfccd14f6f211e
Becky Smith <becky.smith@thedatalab.org>
2026-01-23 10:47:14 +0000

    Move summarize_csv to utils module

--- airlock/utils.py
+++ airlock/utils.py
@@ -1,3 +1,5 @@
+from collections import Counter
+from itertools import zip_longest
 from pathlib import Path
 from typing import IO
 
@@ -35,3 +37,112 @@ def truncate_log_stream(stream: IO[str], n: int):
     span.set_attribute("job.log_truncated", truncated)
 
     return log, truncated
+
+
+def _is_not_divisible_by(value: int | float, divider: int):
+    return value % divider != 0
+
+
+def _is_not_midpoint6_rounded(value: int | float):
+    return value != 0 and ((value - 3) % 6 != 0)
+
+
+def summarize_column(column_name: str, column_data: tuple[str]):
+    # Likely missing/null/redacted values removed for checking for type and for numeric summaries
+    missing_strings = ["", "null", "none"]
+    # redacted strings counted separately for numeric columns
+    redacted_strings = ["[redacted]", "redacted", "na", "n/a"]
+
+    # strip whitespace, lower, and count; ignore non-None, these only occur for
+    # CSVs with uneven columns
+    counter = Counter(i.strip().lower() for i in column_data if i is not None)
+    non_missing_counter = {
+        val: count
+        for val, count in counter.items()
+        if val not in missing_strings + redacted_strings
+    }
+
+    try:
+        numeric_data = {int(i): count for i, count in non_missing_counter.items()}
+        type_ = "integer"
+    except ValueError:
+        try:
+            numeric_data = {float(i): count for i, count in non_missing_counter.items()}
+            type_ = "float"
+        except ValueError:
+            type_ = "text"
+            numeric_data = None
+
+    column_summary = {
+        "column_name": column_name,
+        "type": type_,
+        "total_rows": len(column_data),
+        "missing_values": sum(
+            count for val, count in counter.items() if val in missing_strings
+        ),
+        # defaults for numeric calculations
+        "redacted": "-",
+        "min": "-",
+        "min_gt_0": "-",
+        "max": "-",
+        "sum": "-",
+        "divisible_by_5": "-",
+        "divisible_by_6": "-",
+        "midpoint6_rounded": "-",
+    }
+
+    if numeric_data is not None:
+        column_summary.update(
+            {
+                "redacted": sum(
+                    count for val, count in counter.items() if val in redacted_strings
+                ),
+                "min": min(numeric_data),
+                "min_gt_0": min(abs(i) for i in set(numeric_data) if i > 0),
+                "max": max(numeric_data),
+                "sum": sum(i * count for i, count in numeric_data.items()),
+                "divisible_by_5": not any(
+                    _is_not_divisible_by(i, 5) for i in numeric_data
+                ),
+                # https://docs.opensafely.org/outputs/sdc/#midpoint-6-rounding
+                # Divisible by 6 indicates midpoint 6 derived (0, 6, 12...)
+                # midpoint 6 takes values 0, 3, 9, 15...)
+                "divisible_by_6": not any(
+                    _is_not_divisible_by(i, 6) for i in numeric_data
+                ),
+                "midpoint6_rounded": not any(
+                    _is_not_midpoint6_rounded(i) for i in numeric_data
+                ),
+            }
+        )
+
+    return column_summary
+
+
+def summarize_csv(column_names: list[str], enumerated_rows: list[tuple[str]]):
+    if not enumerated_rows:
+        return
+    # Get just the row values, without the row number
+    row_values = list(zip(*enumerated_rows))[1]
+    # Get a list of values for each column, allowing for odd CSVs with rows that are shorter than the header row
+    column_values = list(zip_longest(*row_values))
+    return {
+        "headers": [
+            "Column name",
+            "Column type",
+            "Total rows",
+            "Null / missing",
+            "Redacted",
+            "Min value",
+            "Min non-zero",
+            "Max value",
+            "Sum",
+            "Divisible by 5",
+            "Divisible by 6",
+            "Midpoint 6 rounded",
+        ],
+        "rows": [
+            list(summarize_column(column_name, column_values[i]).values())
+            for i, column_name in enumerate(column_names)
+        ],
+    }
